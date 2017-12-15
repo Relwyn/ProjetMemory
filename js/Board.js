26303f484daea92471f457f232482a211e6e182e
Relwyn <maerten.nicolas62@gmail.com>
2017-12-15 14:50:32 +0100

    2

--- js/Board.js
+++ js/Board.js
@@ -1,5 +1,46 @@
 
-function Board(){
+
+function randomInt(mini, maxi) {
     'use strict';
-    this.cartes =["1CA", "1CO", "1P", "1T", "2CA", "2CO", "2P", "2T", "3CA", "3CO", "3P", "3T", "1CA", "1CO", "1P", "1T", "2CA", "2CO", "2P", "2T", "3CA", "3CO", "3P", "3T", "joker"];
+    var nb = mini + (maxi + 1 - mini) * Math.random();
+    return Math.floor(nb);
 }
+
+Array.prototype.shuffle = function (n) {
+    'use strict';
+    if (!n)
+        n = this.length;
+    if (n > 1) {
+        var i = randomInt(0, n - 1);
+        var tmp = this[i];
+        this[i] = this[n - 1];
+        this[n - 1] = tmp;
+        this.shuffle(n - 1);
+    }
+};
+
+
+function Board() {
+    'use strict';
+    var tab = document.getElementById('grille');
+    var i, j;
+    var grille = new Array();
+    for (i = 0; i < 9; i++) {
+        grille[i] = new Array();
+    }
+    this.cartes = ["1CA", "1CO", "1P", "1T", "2CA", "2CO", "2P", "2T", "3CA", "3CO", "3P", "3T", "1CA", "1CO", "1P", "1T", "2CA", "2CO", "2P", "2T", "3CA", "3CO", "3P", "3T", "joker"];
+    this.cartes.shuffle();
+    for (i = 0; i < 5; i += 1) {
+        var tr = document.createElement('tr');
+
+        for (j = 0; j < 5; j += 1) {
+            var td = document.createElement('td');
+            grille[i][j] = new Tile(i, j, this.cartes[((i + 1) * (j + 1))]);
+            td.appendChild(grille[i][j].img);
+            tr.appendChild(td)
+        }
+        tab.appendChild(tr);
+    }
+
+}
+
---
+++ js/main.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Nico on 15/12/2017.
+ */
+B = new Board();
