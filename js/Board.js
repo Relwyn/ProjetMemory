d8c6a33b9494db71a88ffa3467fe7d0baef425c3
Relwyn <maerten.nicolas62@gmail.com>
2017-12-15 14:07:31 +0100

    Initial commit

---
+++ js/Board.js
@@ -0,0 +1,5 @@
+
+function Board(){
+    'use strict';
+    this.cartes =["1CA", "1CO", "1P", "1T", "2CA", "2CO", "2P", "2T", "3CA", "3CO", "3P", "3T", "1CA", "1CO", "1P", "1T", "2CA", "2CO", "2P", "2T", "3CA", "3CO", "3P", "3T", "joker"];
+}
---
+++ js/Tile.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Nico on 14/12/2017.
+ */
+
+function Tile(x,y,src) {
+    'use strict';
+    this.x = x;
+    this.y = y;
+    this.retourne = false;
+    this.src = src;
+    this.img = document.createElement("img");
+    this.img.src = './img/dos.png';
+    this.img.with = 110;
+    this.img.height = 160;
+}
+
+Tile.prototype.setCoord = function (x, y) {
+    'use strict';
+    this.x = x;
+    this.y = y;
+};
+
+Tile.prototype.display = function () {
+    'use strict';
+    if (this.retourne){
+        this.img.src = this.src
+    }
+    else{
+        this.img.src = './img/dos.png'
+    }
+
+};
+
+Tile.prototype.flipTile = function () {
+    'use strict';
+    this.retourne = !this.retourne;
+};
